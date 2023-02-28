67096439a8266dcff935e15c0d4d062e8a9391e3
keerthx <officialprakeerthjm@gmail.com>
2023-02-28 10:07:55 +0530

    Updated button

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,19 @@
 quotes=[
-    "We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology.– Carl Sagan ",
-    "Everything is theoretically impossible until it is done. – Robert A. Heinlein",
-    "The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom. – Isaac Asimov",
-    "If we knew what it was we were doing, it would not be called research, would it? – Albert Einstein"
+    "We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology. ",
+    "Everything is theoretically impossible until it is done. ",
+    "The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.",
+    "If we knew what it was we were doing, it would not be called research, would it?"
+]
+
+quoteby = [
+    "– Carl Sagan",
+    "– Robert A. Heinlein",
+    " – Isaac Asimov",
+    " – Albert Einstein"
 ]
 
 function getQuote() {
     var randomNumber = Math.floor(Math.random()* quotes.length) 
     document.getElementById('newQuoteSection').innerHTML = quotes[randomNumber]
+    document.getElementById('newQuotebySection').innerHTML = quoteby[randomNumber]
 }
